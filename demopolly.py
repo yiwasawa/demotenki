d7d6a5b85a922aed832f0f8c83c53e459c9cbef1
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-19 12:34:45 +0900

    test

--- demopolly.py
+++ demopolly.py
@@ -1,6 +1,9 @@
 from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
 import os
 import sys
+import subprocess
 
 session = Session(region_name="ap-northeast-1")
 polly = session.client("polly")
