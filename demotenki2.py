1a339fa189f19965bdd994fcdade3c10da03e228
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-20 11:35:51 +0900

    test

--- demotenki2.py
+++ demotenki2.py
@@ -1,12 +1,16 @@
-﻿from bottle import *
+﻿import os
+import sys
+
+sys.path.append('/home/ec2-user/.pyenv/versions/3.6.2/lib/python3.6/site-packages')
+
+from bottle import *
 import pyodbc
 from password.password import *
 
 from boto3 import Session
 from botocore.exceptions import BotoCoreError, ClientError
 from contextlib import closing
-import os
-import sys
+
 import subprocess
 
 # 表紙
