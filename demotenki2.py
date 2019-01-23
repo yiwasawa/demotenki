90aaa7f524246d8abe2365486e9b6462863e8210
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-23 21:10:11 +0900

    test

--- demotenki2.py
+++ demotenki2.py
@@ -269,6 +269,13 @@ def mc_sendassetfrom():
     # return json.dumps(result)
     return data_multi1
 
+# サンプル原稿取得
+@route('/mc_sendassetfrom', method='GET')
+def getsamplescript():
+
+
+    return '[{"fromaddress":"1TgN4QggTGgyN8hPcda5qQPA5t2kjj9zXpxMhA","toaddress":"1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s","currency":CURRENCY, "qty":"10"}]'
+
 # run(host='0.0.0.0', port=8080, debug=True, reloader=True)
 # run(host='13.113.245.130', port=80, debug=True, reloader=True)
 # run(host='0.0.0.0', port=8080, debug=True, reloader=True)
