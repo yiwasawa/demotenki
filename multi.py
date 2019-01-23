eab42a7bcc89621c707de6f0a0358c08469e30d3
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-23 18:55:05 +0900

    test

---
+++ multi.py
@@ -0,0 +1,13 @@
+import requests
+import time
+import json
+import datetime
+import pyodbc
+from password.password import *
+
+headers = {'apikey':MULTIAPIKEY}
+payload = {'method':'getaddressbalances','params':['1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s']}
+response_multi1 = requests.post(MULTIENDPOINT, data=json.dumps(payload), headers=headers)
+
+data_multi1 = response_multi1.json()
+
