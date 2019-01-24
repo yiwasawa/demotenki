7a3f89012dc3d989fab65a6e78f3f2ebd10cd5e6
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-24 15:40:15 +0900

    test

--- multi.py
+++ multi.py
@@ -18,7 +18,10 @@ print(data_multi1["result"])
 print("その2")
 print(data_multi1["result"]["1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s"])
 print("その3")
+
 print(data_multi1["result"]["1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s"][0]["qty"])
+print(data_multi1["result"]["1TgN4QggTGgyN8hPcda5qQPA5t2kjj9zXpxMhA"][0]["qty"])
+print(data_multi1["result"]["163bVJBXpwrBZX3qsv8aBf4S2KJHaQiPCj2zAV"][0]["qty"])
 
 for key in data_multi1:
   print(key)
