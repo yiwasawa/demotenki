0221d808b4995f435acb226cfe13fa771cfc7480
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-01-24 15:57:16 +0900

    test

--- multi.py
+++ multi.py
@@ -5,6 +5,7 @@ import binascii
 
 headers = {'apikey':MULTIAPIKEY}
 
+# ※注意※これはスタブプログラムです。
 
 payload = {'method':'getmultibalances','params':['*','MKICoins']}
 response_multi1 = requests.post(MULTIENDPOINT, data=json.dumps(payload), headers=headers)
@@ -20,8 +21,9 @@ data_multi2 = response_multi2.json()
 data = data_multi2["result"][0]["data"]
 accountname = binascii.unhexlify(data).decode('utf-8')
 
-print(accountname)
-print(data_multi1["result"]["1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s"][0]["qty"])
+accountname_1 = accountname
+address_1 = "1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s"
+qty_1 = data_multi1["result"]["1PN6wchQ348Rn8rW5qgbgQZAytZeSuuF8oyT3s"][0]["qty"]
 
 # 2件目
 
@@ -31,8 +33,9 @@ data_multi2 = response_multi2.json()
 data = data_multi2["result"][0]["data"]
 accountname = binascii.unhexlify(data).decode('utf-8')
 
-print(accountname)
-print(data_multi1["result"]["1TgN4QggTGgyN8hPcda5qQPA5t2kjj9zXpxMhA"][0]["qty"])
+accountname_2 = accountname
+address_2 = "1TgN4QggTGgyN8hPcda5qQPA5t2kjj9zXpxMhA")
+qty_2 = data_multi1["result"]["1TgN4QggTGgyN8hPcda5qQPA5t2kjj9zXpxMhA"][0]["qty"]
 
 # 3件目
 
@@ -42,6 +45,21 @@ data_multi2 = response_multi2.json()
 data = data_multi2["result"][0]["data"]
 accountname = binascii.unhexlify(data).decode('utf-8')
 
-print(accountname)
-print(data_multi1["result"]["163bVJBXpwrBZX3qsv8aBf4S2KJHaQiPCj2zAV"][0]["qty"])
+accountname_3 = accountname
+address_3 = "163bVJBXpwrBZX3qsv8aBf4S2KJHaQiPCj2zAV"
+qty_3 = data_multi1["result"]["163bVJBXpwrBZX3qsv8aBf4S2KJHaQiPCj2zAV"][0]["qty"]
 
+print(accountname_1)
+print(address_1)
+print(qty_1)
+print(accountname_2)
+print(address_2)
+print(qty_2)
+print(accountname_3)
+print(address_3)
+print(qty_3)
+
+
+
+
+return json.dumps(testdata)
