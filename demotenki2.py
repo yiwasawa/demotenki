547b499aad8db2f2900ff1af5824685ea630d525
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-04-05 17:12:40 +0900

    test

--- demotenki2.py
+++ demotenki2.py
@@ -417,13 +417,15 @@ def sc_get():
     payload = {'method':'liststreamkeyitems','params':['demo', '1904050001', False]}
     response_sc1 = requests.post(MULTIENDPOINT, data=json.dumps(payload), headers=headers)
 
-
     data_sc1 = response_sc1.json()
 
     print(response_sc1)
     print(type(response_sc1))
     print(data_sc1)
-    print(type(data_sc1))
+    print(type(data_sc1)) #dict型
+
+    list = data_sc1.keys()
+    print list
 
     # qty = json.dumps(data_multi1["result"][0]["qty"])
     # result = {"qty":qty}
