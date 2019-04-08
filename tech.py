0d06188db3b65c89364e08dc60b5bcc396c4f8ba
yiwasawa <iwasawa.yoshikazu@gmail.com>
2019-04-08 09:32:05 +0900

    test

---
+++ tech.py
@@ -0,0 +1,20 @@
+﻿import os
+import sys
+
+sys.path.append('/home/ec2-user/.pyenv/versions/3.6.2/lib/python3.6/site-packages')
+
+from bottle import *
+import pyodbc
+from password.password import *
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+
+import subprocess
+import json
+import datetime
+import requests
+import binascii
+
+print("test")
+
